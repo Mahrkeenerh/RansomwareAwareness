22a7f846db167576d1b1e281418b603692176e77
Mahrkeenerh <samuelbuban@gmail.com>
2021-03-02 14:02:06 +0100

    Python export

--- awareness.py
+++ awareness.py
@@ -1,5 +1,8 @@
 import webbrowser
 
 
-url = 'http://docs.python.org/'
-webbrowser.open(url, new=0, autoraise=True)
+form = 'https://forms.gle/Tm2e2KPPNwGtcz1C9'
+git = 'https://github.com/Mahrkeenerh/RansomwareAwareness/tree/master/Awareness'
+
+webbrowser.open(form, new=0, autoraise=True)
+webbrowser.open(git, new=0, autoraise=True)
