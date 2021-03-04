ebd8c186ed797292126f7aa2e57bd6a036ed93c4
Mahrkeenerh <samuelbuban@gmail.com>
2021-03-04 22:38:24 +0100

    Added sleep

--- awareness.py
+++ awareness.py
@@ -1,8 +1,10 @@
 import webbrowser
+from time import sleep
 
 
 form = 'https://forms.gle/Tm2e2KPPNwGtcz1C9'
 git = 'https://github.com/Mahrkeenerh/RansomwareAwareness/tree/master/Info'
 
 webbrowser.open(form, new=0, autoraise=True)
+sleep(0.1)
 webbrowser.open(git, new=0, autoraise=True)
